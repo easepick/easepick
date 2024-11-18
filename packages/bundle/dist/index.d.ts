d3a5a3e65106153aa2a087cd9538053e2ac02466
Spencer Markowski <spencer.markowski@forusall.com>
2024-11-17 19:31:01 -0500

    Add bundle dist

---
+++ packages/bundle/dist/index.d.ts
@@ -0,0 +1,10 @@
+import { DateTime } from '@easepick/datetime';
+import { easepick, create } from '@easepick/core';
+import { LockPlugin } from '@easepick/lock-plugin';
+import { PresetPlugin } from '@easepick/preset-plugin';
+import { RangePlugin } from '@easepick/range-plugin';
+import { TimePlugin } from '@easepick/time-plugin';
+import { KbdPlugin } from '@easepick/kbd-plugin';
+import { AmpPlugin } from '@easepick/amp-plugin';
+import './index.scss';
+export { easepick, create, DateTime, LockPlugin, PresetPlugin, RangePlugin, TimePlugin, KbdPlugin, AmpPlugin, };
